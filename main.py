0d330773a2348014668cacd79c73c8efee2bf686
Ankitrana123456 <ankitrana73jio@gmail.com>
2025-08-27 09:27:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ async def upload(bot: Client, m: Message):
              #id =  url.split("/")[-2]
              #url = f"https://anonymouspwplayer-f996115ea61a.herokuapp.com/pw?url={url}?token={raw_text4}"
             #url = f"https://madxapi-d0cbf6ac738c.herokuapp.com/{id}/master.m3u8?token={raw_text4}"
-            elif "0e5a3f512dec" in url or "sec1.pw.live" in url:
+            elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
              url = f"https://anonymouspwplayer-0e5a3f512dec.herokuapp.com/pw?url={url}&token={raw_text4}"
             elif '/master.mpd' in url:
              id =  url.split("/")[-2]
